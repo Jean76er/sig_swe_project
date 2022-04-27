249a71db0ba0d5cce9e1016009eeef5e22557dad
Jean Torres <jctorres4193@gmail.com>
2022-04-27 17:09:01 -0400

    almsot done

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ First project that I am working on in ACM club. Spotify web app that returns top
 How to run
 - Start the virtual env with command: tutorial-env\scripts\activate
 - pip install Flask
-- Link to website. 127.0.0.1:5000
+- Link to website. 127.0.0.1:5000 or http://10.202.69.14:8080/
 - To start website app.py
--- app.py
+++ app.py
@@ -1,17 +1,27 @@
+'''This file is the main file for the application.'''
 from flask import Flask, render_template
-app = Flask(__name__)
 from data import get_data
+import random
+import os
+
+app = Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def hello():
-    info = get_data()
+    '''This function is the main function for the application.'''
+
+    '''Artic Monkeys, Bad Bunny, Inner Wave'''
+    artist_list = ['7Ln80lUS6He07XvHI8qqHH', '4q3ewBCX7sLwd24euuV69X', '6AQEfqGPSxZX0nJVonYxi6']
+
+    rand = random.randint(0, len(artist_list) - 1)
+
+    info = get_data(artist_list[rand])
     print(info)
 
-
-
-
     return render_template(
-        'index.html', 
+        'index.html',
         song_name = info[0],
         artist_names = info[1],
         album_image = info[2],
@@ -20,8 +30,9 @@ def hello():
         total_artists = len(info[1])
     )
 
-
 if __name__ == '__main__':
     app.run(
         debug=True,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080))
     )
